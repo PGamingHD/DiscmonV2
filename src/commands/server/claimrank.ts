28c81358101757215dc61f7d0d90f3d04cce04f2
PGamingHD <pontus.2003@hotmail.com>
2023-06-18 16:27:40 +0200

    Added gen 1 and fixes with embeds
    Added gen 1 and fixes with embeds

--- src/commands/server/claimrank.ts
+++ src/commands/server/claimrank.ts
@@ -25,10 +25,10 @@ export default new Command({
 
         if (usersData.trainerRank === TrainerRanks.BRONZE_TRAINER) {
             const bronzeRole: Role | null = await interaction.guild.roles.fetch('1119603015524024350');
-            if (!bronzeRole) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`No \`Bronze Trainer\` role was found, please contact a Staff Member.`)]});
+            if (!bronzeRole) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.RED).setDescription(`No \`Bronze Trainer\` role was found, please contact a Staff Member.`)]});
 
             // @ts-ignore
-            if (interaction.member.roles.cache.has(bronzeRole.id)) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`You already have the \`Bronze Trainer\` rank.`)]});
+            if (interaction.member.roles.cache.has(bronzeRole.id)) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.RED).setDescription(`You already have the \`Bronze Trainer\` rank.`)]});
 
             // @ts-ignore
             await interaction.member.roles.add(bronzeRole);
@@ -36,10 +36,10 @@ export default new Command({
             return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`You have successfully been given the \`Bronze Trainer\` rank, thank you for your support!`)]});
         } else if (usersData.trainerRank === TrainerRanks.SILVER_TRAINER) {
             const silverRole: Role | null = await interaction.guild.roles.fetch('1119603226761769060');
-            if (!silverRole) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`No \`Silver Trainer\` role was found, please contact a Staff Member.`)]});
+            if (!silverRole) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.RED).setDescription(`No \`Silver Trainer\` role was found, please contact a Staff Member.`)]});
 
             // @ts-ignore
-            if (interaction.member.roles.cache.has(silverRole.id)) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`You already have the \`Silver Trainer\` rank.`)]});
+            if (interaction.member.roles.cache.has(silverRole.id)) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.RED).setDescription(`You already have the \`Silver Trainer\` rank.`)]});
 
             // @ts-ignore
             await interaction.member.roles.add(silverRole);
@@ -47,10 +47,10 @@ export default new Command({
             return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`You have successfully been given the \`Silver Trainer\` rank, thank you for your support!`)]});
         } else if (usersData.trainerRank === TrainerRanks.GOLD_TRAINER) {
             const goldRole: Role | null = await interaction.guild.roles.fetch('1119603347989733537');
-            if (!goldRole) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`No \`Gold Trainer\` role was found, please contact a Staff Member.`)]});
+            if (!goldRole) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.RED).setDescription(`No \`Gold Trainer\` role was found, please contact a Staff Member.`)]});
 
             // @ts-ignore
-            if (interaction.member.roles.cache.has(goldRole.id)) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`You already have the \`Gold Trainer\` rank.`)]});
+            if (interaction.member.roles.cache.has(goldRole.id)) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.RED).setDescription(`You already have the \`Gold Trainer\` rank.`)]});
 
             // @ts-ignore
             await interaction.member.roles.add(goldRole);
@@ -58,10 +58,10 @@ export default new Command({
             return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`You have successfully been given the \`Gold Trainer\` rank, thank you for your support!`)]});
         } else if (usersData.trainerRank === TrainerRanks.PLATINUM_TRAINER) {
             const platinumRole: Role | null = await interaction.guild.roles.fetch('1119603423852105758');
-            if (!platinumRole) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`No \`Platinum Trainer\` role was found, please contact a Staff Member.`)]});
+            if (!platinumRole) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.RED).setDescription(`No \`Platinum Trainer\` role was found, please contact a Staff Member.`)]});
 
             // @ts-ignore
-            if (interaction.member.roles.cache.has(platinumRole.id)) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`You already have the \`Platinum Trainer\` rank.`)]});
+            if (interaction.member.roles.cache.has(platinumRole.id)) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.RED).setDescription(`You already have the \`Platinum Trainer\` rank.`)]});
 
             // @ts-ignore
             await interaction.member.roles.add(platinumRole);
@@ -69,10 +69,10 @@ export default new Command({
             return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`You have successfully been given the \`Platinum Trainer\` rank, thank you for your support!`)]});
         } else if (usersData.trainerRank === TrainerRanks.MODERATOR) {
             const modRole: Role | null = await interaction.guild.roles.fetch('1070101977297600654');
-            if (!modRole) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`No \`Moderator\` role was found, please contact a Staff Member.`)]});
+            if (!modRole) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.RED).setDescription(`No \`Moderator\` role was found, please contact a Staff Member.`)]});
 
             // @ts-ignore
-            if (interaction.member.roles.cache.has(modRole.id)) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`You already have the \`Moderator\` rank.`)]});
+            if (interaction.member.roles.cache.has(modRole.id)) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.RED).setDescription(`You already have the \`Moderator\` rank.`)]});
 
             // @ts-ignore
             await interaction.member.roles.add(modRole);
@@ -80,10 +80,10 @@ export default new Command({
             return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`You have successfully been given the \`Moderator\` rank, welcome Staff Member!`)]});
         } else if (usersData.trainerRank === TrainerRanks.ADMINISTRATOR) {
             const adminRole: Role | null = await interaction.guild.roles.fetch('1010999169701392464');
-            if (!adminRole) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`No \`Administrator\` role was found, please contact a Staff Member.`)]});
+            if (!adminRole) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.RED).setDescription(`No \`Administrator\` role was found, please contact a Staff Member.`)]});
 
             // @ts-ignore
-            if (interaction.member.roles.cache.has(adminRole.id)) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`You already have the \`Administrator\` rank.`)]});
+            if (interaction.member.roles.cache.has(adminRole.id)) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.RED).setDescription(`You already have the \`Administrator\` rank.`)]});
 
             // @ts-ignore
             await interaction.member.roles.add(adminRole);
@@ -91,17 +91,17 @@ export default new Command({
             return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`You have successfully been given the \`Administrator\` rank, welcome Staff Member!`)]});
         } else if (usersData.trainerRank === TrainerRanks.DEVELOPER) {
             const devRole: Role | null = await interaction.guild.roles.fetch('1011002806808875089');
-            if (!devRole) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`No \`Developer\` role was found, please contact a Staff Member.`)]});
+            if (!devRole) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.RED).setDescription(`No \`Developer\` role was found, please contact a Staff Member.`)]});
 
             // @ts-ignore
-            if (interaction.member.roles.cache.has(devRole.id)) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`You already have the \`Developer\` rank.`)]});
+            if (interaction.member.roles.cache.has(devRole.id)) return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.RED).setDescription(`You already have the \`Developer\` rank.`)]});
 
             // @ts-ignore
             await interaction.member.roles.add(devRole);
 
             return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`You have successfully been given the \`Developer\` rank, welcome Developer!`)]});
         } else {
-            return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.GREEN).setDescription(`You do not have any Supporter Roles or Staff Roles, is this a mistake contact a Staff Member.`)]});
+            return interaction.reply({ephemeral: true, embeds: [new EmbedBuilder().setColor(Colours.RED).setDescription(`You do not have any Supporter Roles or Staff Roles, is this a mistake contact a Staff Member.`)]});
         }
     },
 });
--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -13,66 +13,7 @@ export default new Event(Events.GuildMemberAdd, async (member) => {
     if (member.guild.id === "1010999169676222514") {
         const cachedChannel: TextChannel = await client.channels.fetch('1053450993301471335') as TextChannel;
 
-        const canvas = createCanvas(1024, 450);
-        const ctx = canvas.getContext('2d');
-
-        ctx.font = "bold 90px Sans";
-        ctx.strokeStyle = "#000000";
-        ctx.lineWidth = 12;
-        ctx.strokeText("WELCOME", 400, 200);
-
-        ctx.fillStyle = "#FFFFFF";
-        ctx.fillRect(0,0, canvas.width, canvas.height);
-        let img = await loadImage("https://wallpaperaccess.com/full/86407.jpg");
-        ctx.drawImage(img, canvas.width / 2 - img.width / 2, canvas.height / 2 - img.height / 2);
-
-        ctx.fillStyle = "#000000";
-        ctx.globalAlpha = 0.5;
-        ctx.fillRect(0,0,25, canvas.height);
-        ctx.fillRect(canvas.width - 25, 0, 25, canvas.height);
-        ctx.fillRect(25, 0, canvas.width - 50, 25);
-        ctx.fillRect(25, canvas.height - 25, canvas.width - 50, 25);
-        ctx.globalAlpha = 1;
-
-        ctx.beginPath();
-        ctx.lineWidth = 10;
-        ctx.strokeStyle = "#e96423";
-        ctx.arc(180, 225, 135, 0, Math.PI * 2, true);
-        ctx.stroke();
-        ctx.closePath();
-        ctx.clip();
-        img = await loadImage(member.displayAvatarURL({extension: "png"}));
-        ctx.drawImage(img, 45, 90, 270, 270);
-        ctx.restore();
-
-        ctx.font = "bold 90px Sans";
-        ctx.strokeStyle = "#000000";
-        ctx.lineWidth = 12;
-        ctx.strokeText("WELCOME", 400, 200);
-        ctx.fillStyle = "#FFFFFF";
-        ctx.fillText("WELCOME", 400, 200);
-
-        ctx.font = "bold 45px Sans";
-        ctx.strokeStyle = "#000000";
-        ctx.lineWidth = 10;
-        ctx.strokeText(`${member.user.username}#${member.user.discriminator}`, canvas.width - 565, canvas.height - 175);
-        ctx.fillStyle = "#eb6123";
-        ctx.fillText(`${member.user.username}#${member.user.discriminator}`, canvas.width - 565, canvas.height - 175);
-
-
-        ctx.font = "bold 40px Sans";
-        ctx.strokeStyle = "#000000";
-        ctx.lineWidth = 10;
-        ctx.strokeText(`Member #${member.guild.memberCount}`, 535, canvas.height - 100);
-        ctx.fillStyle = "#FFFFFF";
-        ctx.fillText(`Member #${member.guild.memberCount}`, 535, canvas.height - 100);
-
-        const attachment: AttachmentBuilder = new AttachmentBuilder(await canvas.encode("png"), {name: `welcome-${member.id}.png`});
-
-        return cachedChannel.send({
-            content: `Welcome ${member}, to **${member.guild.name}**!\nPlease make sure to also read the rules in <#1010999256166957258> before starting to chat.`,
-            files: [attachment]
-        })
+        return cachedChannel.send({content: `Hey ${member}, welcome to **${member.guild.name}**!\nPlease make sure to also read the rules in <#1010999256166957258> before starting to chat.`});
     }
     return;
 });
