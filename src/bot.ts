25dca02215fd7a371216aded236f5829562273ee
PGamingHD <pontus.2003@hotmail.com>
2024-02-17 19:27:35 +0100

    Remade events into Enums to simplify understanding of code
    Remade event strings into Enums to simplify understanding of code

--- src/bot.ts
+++ src/bot.ts
@@ -1,3 +1,4 @@
+import { Events } from "discord.js";
 import { ExtendedClient } from "./structures/Client";
 import logger from "./utils/logger";
 
@@ -5,9 +6,9 @@ export const client = new ExtendedClient();
 
 client.start();
 
-client.on("warn", (e: any) => logger.warning(e));
-client.on("debug", (e: any) => logger.debug(e));
-client.on("error", (e: any) => logger.warning(e));
+client.on(Events.Warn, (e: any) => logger.warning(e));
+client.on(Events.Debug, (e: any) => logger.debug(e));
+client.on(Events.Error, (e: any) => logger.warning(e));
 
 process.on("unhandledRejection", (e: any) => logger.error(e));
 process.on("uncaughtException", (e: any) => logger.error(e));
