ff4003bafd2f2adb99a79e2edc7ee14627507de2
PGamingHD <pontus.2003@hotmail.com>
2025-02-16 02:03:14 +0100

    Added extra logging
    Added extra logging

--- src/bot.ts
+++ src/bot.ts
@@ -6,9 +6,15 @@ export const client = new ExtendedClient();
 
 client.start();
 
+//CLIENT
 client.on(Events.Warn, (e: any) => logger.warning(e));
 client.on(Events.Debug, (e: any) => logger.debug(e));
 client.on(Events.Error, (e: any) => logger.warning(e));
 
+//SHARDS
+client.on(Events.ShardError, (e: any) => logger.error(e));
+client.on(Events.ShardDisconnect, (e: any) => logger.error(e));
+
+//PROCESS
 process.on("unhandledRejection", (e: any) => logger.error(e));
 process.on("uncaughtException", (e: any) => logger.error(e));
